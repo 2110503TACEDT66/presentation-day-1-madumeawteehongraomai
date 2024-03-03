28d34b77ce669d56c83290a37c7b795dc9d9c55e
TamaoSan <thanakitmoss@gmail.com>
2024-03-04 06:08:07 +0700

    add Router of companies

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const dotev = require('dotenv');
+const dotenv = require('dotenv');
+
+//Route files
+const companies = require('./routes/companies');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
 const app=express();
+//Mount routers
+app.use('/api/v1/companies',companies);
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
+app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
