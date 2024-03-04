a74c3d656e6c8be81769eccf606be9a4082b1e8b
TamaoSan <thanakitmoss@gmail.com>
2024-03-04 11:26:22 +0700

    forget something

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,34 +24,40 @@ exports.register=async (req,res,next)=>{
         res.status(400).json({success:false});
         console.log(err.stack);
     }
+};
     //@desc     Login user
 //@route    POST /api/v1/auth/login
 //@access   Public
 exports.login=async (req,res,next)=> {
-    const {email, password}=req.body;
+    try{
+        const {email, password}=req.body;
 
-    //Validate email & password
-    if(!email || !password){
-        return res.status(400).json({success:false,msg:'Please provide an email and password'});
+        //Validate email & password
+        if(!email || !password){
+            return res.status(400).json({success:false,msg:'Please provide an email and password'});
+        }
+
+        //Check for user
+        const user = await User.findOne({email}).select('+password');
+        if(!user){
+            return res.status(400).json({success:false,msg:'Invalid credentials'});
+        }
+
+        //Check if password matcher
+        const isMatch = await user.matchPassword(password);
+
+        if(!isMatch){
+            return res.status(401).json({success:false,msg:'Invalid credentials'});
+        }
+
+        //Create token
+        // const token=user.getSignedJwtToken();
+        // res.status(200).json({success:true,token});
+        sendTokenResponse(user,200,res);
+
+    } catch(err){
+        return res.status(401).json({success:false, msg:'Cannot convert email or password to string'});
     }
-
-    //Check for user
-    const user = await User.findOne({email}).select('+password');
-    if(!user){
-        return res.status(400).json({success:false,msg:'Invalid credentials'});
-    }
-
-    //Check if password matcher
-    const isMatch = await user.matchPassword(password);
-
-    if(!isMatch){
-        return res.status(401).json({success:false,msg:'Invalid credentials'});
-    }
-
-    //Create token
-    // const token=user.getSignedJwtToken();
-    // res.status(200).json({success:true,token});
-    sendTokenResponse(user,200,res);
 };
 
 //Get token from model, create cookie and send response
@@ -83,4 +89,3 @@ exports.getMe=async (req,res,next)=> {
         data:user
     });
 };
-}
