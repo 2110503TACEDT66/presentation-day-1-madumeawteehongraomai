ae4c732bb968a8c9a7797f810d5db762c75704cc
TaritArtchana <beer.tarit@gmail.com>
2024-03-04 08:52:55 +0700

    user can register to system

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 
 //Route files
 const companies = require('./routes/companies');
+const auth = require('./routes/auth');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -13,6 +14,7 @@ connectDB();
 const app=express();
 //Mount routers
 app.use('/api/v1/companies',companies);
+app.use('/api/v1/auth', auth);
 
 const PORT=process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
