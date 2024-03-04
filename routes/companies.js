bb2300036f44f3b40c48be671f33d1e0b30fafcd
TaritArtchana <beer.tarit@gmail.com>
2024-03-04 09:24:16 +0700

    advanced Filtering

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const {protect} = require('../middleware/auth');
+const {protect,authorize} = require('../middleware/auth');
+
+router.route('/').get(getCompanies).post(protect,createCompany);
+router.route('/:id').get(getCompany).put(protect,updateCompany).delete(protect,deleteCompany);
+router.route('/').get(getCompanies).post(protect,authorize('admin'),createCompany);
+router.route('/:id').get(getCompany).put(protect,authorize('admin'),updateCompany).delete(protect,authorize('admin'),deleteCompany);
+
+
 router.route('/').get(getCompanies).post(createCompany);
 router.route('/:id').get(getCompany).put(updateCompany).delete(deleteCompany);
 const {protect} = require('../middleware/auth');
